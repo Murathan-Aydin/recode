910c3914d387fac2130fce2c945c97a0abd6aab9
Mehdi <itsnehco@gmail.com>
2025-03-19 16:27:41 +0100

    fix

--- app/api/route.ts
+++ app/api/route.ts
@@ -8,13 +8,13 @@ export async function POST(request: NextRequest) {
     console.log(tweet);
     
 
-    const data = await connection.query("INSERT INTO tweets (user_id, parent_id, type, tweet) VALUES (?, ?, ?, ?)", [1, null, "original", tweet]); 
+    const data = await connection.query("INSERT INTO tweets (user_id, parent_id, type, tweet) VALUES (?, ?, ?, ?)", [5, null, "original", tweet]); 
 
     return NextResponse.json(data);
 }
 
 export async function GET() {
-    const data = await connection.query("SELECT * FROM tweets WHERE type != ? LIMIT 10", ["reply"]); 
+    const [data] = await connection.query("SELECT * FROM tweets WHERE type != ? ORDER BY id DESC LIMIT 10", ["reply"]); 
 
     return NextResponse.json(data);
 }
