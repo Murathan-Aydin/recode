e43e3d90d6733bfd2222014c47e10cbec97d0c23
murathan aydin <murathan.aydin@epitech.eu>
2025-03-19 16:14:23 +0100

    visual and submit fonction

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,40 @@
-import Image from "next/image";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+    const [tweet, setTweet] = useState("");
+
+    useEffect(() => {
+        
+    }, []);
+
+    const submitTweet =  async (e: any) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+        const response = await fetch("/api/submit", {
+            method: "POST",
+            body: formData,
+        });
+        const data = await response.json();
+        console.log(data);
+    };
+
+    const handleChange = (e: any) => {
+        setTweet(e.target.value);
+    };
+
     return (
         <>
-            <h1>f</h1>
+            <form onSubmit={submitTweet} action="">
+                <input
+                    name="tweet"
+                    value={tweet}
+                    onChange={handleChange}
+                    type="text"
+                    placeholder="tweet"
+                />
+                <button type="submit">Tweet</button>
+            </form>
         </>
     );
 }
