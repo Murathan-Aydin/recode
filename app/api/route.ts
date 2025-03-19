ad24278066bd703195363dcc46fbf5de3077a4f9
murathan aydin <murathan.aydin@epitech.eu>
2025-03-19 16:23:27 +0100

    add connection on db

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,16 +1,17 @@
 import { NextResponse, NextRequest } from "next/server";
+import { connection } from "@/lib/db";
 
 export async function POST(request: NextRequest) {
     const body = await request.json();
     const { tweet } = body;
 
-    const data = await db.query("INSERT INTO tweets (user_id, parent_id, type, tweet) VALUES (?, ?, ?, ?)", [1, null, "orignal", tweet]); 
+    const data = await connection.query("INSERT INTO tweets (user_id, parent_id, type, tweet) VALUES (?, ?, ?, ?)", [1, null, "orignal", tweet]); 
 
     return NextResponse.json(data);
 }
 
 export async function GET() {
-    const data = await db.query("SELECT * FROM tweets WHERE type != ? LIMIT 10", ["reply"]); 
+    const data = await connection.query("SELECT * FROM tweets WHERE type != ? LIMIT 10", ["reply"]); 
 
     return NextResponse.json(data);
 }
