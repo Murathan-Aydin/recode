84f01b28d33ce1bdebfdb6eaa1316de8876a9bca
murathan aydin <murathan.aydin@epitech.eu>
2025-03-19 16:23:16 +0100

    fix db

--- lib/db.ts
+++ lib/db.ts
@@ -1,13 +1,9 @@
-import mysql from 'mysql2/promise'
-import { NextResponse } from 'next/server';
+import mysql from "mysql2/promise";
+import { NextResponse } from "next/server";
 
-
-export async function GET(request: Request) {
-    const connection = await mysql.createConnection({
-        host: '198.244.233.0',
-        user: 'twitter',
-        password: 'twitter.epitech',
-        database: 'twitter_db'
-    });
-
-}
+export const connection = await mysql.createConnection({
+    host: "198.244.233.0",
+    user: "twitter",
+    password: "twitter.epitech",
+    database: "twitter_db",
+});
